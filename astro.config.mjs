394ce0641596943808e531f69e02bfe37b8e84f9
majamaja282 <maya.sediva@gmail.com>
2024-06-18 06:06:44 +0000

    přidali jsme footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tomaskebrle.github.io',
+	site: 'https://majamaja282.github.io',
 	base: '/astro-test',
 	integrations: [mdx(), sitemap()],
 });
